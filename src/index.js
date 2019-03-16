34cf3870169983ad7c7391ec1906744fc725f777
Christopher Wenceslaus <topher.wenceslaus@gmail.com>
2019-03-17 00:43:27 +0530

    1.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const useGeolcation= ()=>{
+    const [coords, setCoordinates] = useState({})
+
+    useEffect(()=>{
+           window.navigator.geolocation.getCurrentPosition((coords)=>{
+                let coordss = {
+                        lat: coords.coords.latitude,
+                        lon: coords.coords.longitude
+                } 
+                setCoordinates(coordss)
+           },()=>{
+               console.log("permission denied")
+           }) 
+    },[])  
+    return coords
+}
+
+const App =()=>{
+    const coords = useGeolcation()
+    console.log(coords)
+    return (<h1>
+            {coords.lat} <br/>
+            {coords.lon}
+    </h1>)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
